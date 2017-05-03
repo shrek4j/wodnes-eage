d8a1192663274654689fd28ca8a8b70271b1b1ff
shrek4j <scxtjwgy2005@163.com>
2017-05-03 14:22:52 +0800

    3

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,4 +2,6 @@
 return array(
 	//'配置项'=>'配置值'
     'DB_CONFIG2' => 'mysql://root:Password123@localhost:3306/lexi_db#utf8',
+    'MODULE_ALLOW_LIST'    =>    array('Home'),
+    'DEFAULT_MODULE'       =>    'Home',
 );

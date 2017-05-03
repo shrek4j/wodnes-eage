ba2881e3d81923b25d76dfb20c41c7a912df9477
shrek4j <scxtjwgy2005@163.com>
2017-05-03 11:35:01 +0800

    1

--- Application/Home/Behaviors/ClientCheckBehavior.class.php
+++ Application/Home/Behaviors/ClientCheckBehavior.class.php
@@ -5,9 +5,15 @@ class ClientCheckBehavior extends \Think\Behavior{
     //行为执行入口
     public function run(&$return){
     	
+    	//设置默认默认主题为 Mobile
+        C('DEFAULT_V_LAYER','Mobile');
+
         if (ismobile()) {
-            //设置默认默认主题为 Mobile
-            C('DEFAULT_V_LAYER','Mobile');
+        	session('isMobile','1');
+        	//I('session.isMobile',1); 
+        }else{
+        	session('isMobile','0');
+        	//I('session.isMobile',0); 
         }
         $return = true;
     }

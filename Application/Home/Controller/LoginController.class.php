64240ecc81d73b6bf6874cbbc41f8e83236a62e1
shrek4j <scxtjwgy2005@163.com>
2017-09-13 17:40:43 +0800

    5

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 class LoginController extends Controller {
     
 
-    public function onLogin($code){  
+    public function onLogin($code){
         $APPID = "wx4ffd6dcb1f7a62c4";
         $SECRET = "3c53ecb5f0b6a6517ad9224a22db32d3";
         $wxurl = "https://api.weixin.qq.com/sns/jscode2session?appid=".$APPID."&secret=".$SECRET."&js_code=".$code."&grant_type=authorization_code";
@@ -14,10 +14,10 @@ class LoginController extends Controller {
         // 设置你需要抓取的URL 
         curl_setopt($curl, CURLOPT_URL, $wxurl); 
         // 设置header 响应头是否输出
-        curl_setopt($curl, CURLOPT_HEADER, 1); 
+        curl_setopt($curl, CURLOPT_HEADER, 0); 
         // 设置cURL 参数，要求结果保存到字符串中还是输出到屏幕上。
         // 1如果成功只将结果返回，不自动输出任何内容。如果失败返回FALSE 
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0); 
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
         // 运行cURL，请求网页 
         $wxdata = curl_exec($curl); 
         // 关闭URL请求 
@@ -33,7 +33,7 @@ class LoginController extends Controller {
         }
         $userId = $user[0]['id'];
 
-        $result = array("wxdata"=>$wxdata,"userId"=>$userId);
+        $result = array("userId"=>$userId);
         $data = json_encode($result);
         $this->ajaxReturn($data);
     }

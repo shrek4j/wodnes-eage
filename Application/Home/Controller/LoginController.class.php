9313b09042fb2a162e13df3b916f779f7f002c62
shrek4j <scxtjwgy2005@163.com>
2017-09-13 15:27:20 +0800

    login

---
+++ Application/Home/Controller/LoginController.class.php
@@ -0,0 +1,30 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class LoginController extends Controller {
+    $APPID = "wx4ffd6dcb1f7a62c4";
+    $SECRET = "3c53ecb5f0b6a6517ad9224a22db32d3";
+
+    public function onLogin($code){  
+        $wxurl = "https://api.weixin.qq.com/sns/jscode2session?appid=".$APPID."&secret=".$SECRET."&js_code=".$code."&grant_type=authorization_code";
+
+        // 初始化一个 cURL 对象 
+        $curl = curl_init(); 
+        // 设置你需要抓取的URL 
+        curl_setopt($curl, CURLOPT_URL, $wxurl); 
+        // 设置header 响应头是否输出
+        curl_setopt($curl, CURLOPT_HEADER, 1); 
+        // 设置cURL 参数，要求结果保存到字符串中还是输出到屏幕上。
+        // 1如果成功只将结果返回，不自动输出任何内容。如果失败返回FALSE 
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0); 
+        // 运行cURL，请求网页 
+        $wxdata = curl_exec($curl); 
+        // 关闭URL请求 
+        curl_close($curl);
+
+        $result = array("wxdata"=>$wxdata);
+        $data = json_encode($result);
+        $this->ajaxReturn($data);
+    }
+
+}
